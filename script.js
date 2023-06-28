eb1c1dfc50cb78757e1e6195a1456742ff07c6de
l4te-fl4ker <136440740+l4te-fl4ker@users.noreply.github.com>
2023-06-28 19:33:29 +1200

    add number and clear button input display on click

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let ans = '';
+let operation = '';
+let display = document.querySelector('.calculator-display');
+const zeroBtn = document.querySelector('#zero-btn');
+const oneBtn = document.querySelector('#one-btn');
+const twoBtn = document.querySelector('#two-btn');
+const threeBtn = document.querySelector('#three-btn');
+const fourBtn = document.querySelector('#four-btn');
+const fiveBtn = document.querySelector('#five-btn');
+const sixBtn = document.querySelector('#six-btn');
+const sevenBtn = document.querySelector('#seven-btn');
+const eightBtn = document.querySelector('#eight-btn');
+const nineBtn = document.querySelector('#nine-btn');
+const clearBtn = document.querySelector('#clear-btn');
+
+zeroBtn.addEventListener('click', () => displayInputs(0));
+oneBtn.addEventListener('click',() => displayInputs(1));
+twoBtn.addEventListener('click', () => displayInputs(2));
+threeBtn.addEventListener('click', () => displayInputs(3));
+fourBtn.addEventListener('click', () => displayInputs(4));
+fiveBtn.addEventListener('click', () => displayInputs(5));
+sixBtn.addEventListener('click', () => displayInputs(6));
+sevenBtn.addEventListener('click', () => displayInputs(7));
+eightBtn.addEventListener('click', () => displayInputs(8));
+nineBtn.addEventListener('click', () => displayInputs(9));
+clearBtn.addEventListener('click', () => clearInput());
+
 function add (a,b) {
   return +a + +b;
 }
@@ -14,10 +43,6 @@ function divide (a,b) {
   return a / b;
 }
 
-let firstNumber = 0;
-let secondNumber = 0;
-let operation = '';
-
 function operate (operator, firstNumber, secondNumber) {
   if (operator === '+') {
     return add(firstNumber,secondNumber);
@@ -30,10 +55,11 @@ function operate (operator, firstNumber, secondNumber) {
   } else return 'Error unknown operation';
 }
 
-let display = document.querySelector('.calculator-display');
-
 function displayInputs (value) {
   display.textContent += `${value}`;
 }
-displayInputs('44');
-displayInputs('33')
+
+function clearInput () {
+  display.textContent = display.textContent.slice(0, display.textContent.length -1);
+}
+
