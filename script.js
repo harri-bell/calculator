67e2384c59d7848337f8be1ca794528b2c1c72dd
l4te-fl4ker <136440740+l4te-fl4ker@users.noreply.github.com>
2023-06-28 15:05:00 +1200

    create operation funtions

---
+++ script.js
@@ -0,0 +1,31 @@
+function add (a,b) {
+  return +a + +b;
+}
+
+function subtract (a,b) {
+  return a - b;
+}
+
+function multiply (a,b) {
+  return a * b;
+}
+
+function divide (a,b) {
+  return a / b;
+}
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operation = '';
+
+function operate (operator, firstNumber, secondNumber) {
+  if (operator === '+') {
+    return add(firstNumber,secondNumber);
+  } else if (operator === '-') {
+    return subtract(firstNumber,secondNumber);
+  } else if (operator === 'x') {
+    return multiply(firstNumber,secondNumber);
+  } else if ( operator === '/') {
+    return divide(firstNumber,secondNumber)
+  } else return 'Error unknown operation';
+}
